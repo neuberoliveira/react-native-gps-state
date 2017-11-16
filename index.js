11f7f61f8612677c07079a7e9966dcaa691eb689
Henrik Raitasola <henrik.raitasola@gmail.com>
2017-11-16 09:21:12 +0200

    Run code with prettier

--- index.js
+++ index.js
@@ -1,65 +1,60 @@
-'use strict'
-
-const {NativeModules, NativeEventEmitter, Platform} = require('react-native');
-const {GPSState} = NativeModules;
+const { NativeModules, NativeEventEmitter, Platform } = require('react-native');
+const { GPSState } = NativeModules;
 
 const gpsStateEmitter = new NativeEventEmitter(GPSState);
-var subscription = null;
-var listener = null;
-const isDroid = Platform.OS=='android';
-const isIOS = Platform.OS=='ios';
+let subscription = null;
+let listener = null;
+const isDroid = Platform.OS == 'android';
+const isIOS = Platform.OS == 'ios';
 
-subscription = gpsStateEmitter.addListener('OnStatusChange', (response)=>{
-	if(listener){
-		var status = null;
-		if(isIOS){
-			status = response;
-		}else{
-			status = response.status;
-		}
+subscription = gpsStateEmitter.addListener('OnStatusChange', (response) => {
+  if (listener) {
+    let status = null;
+    if (isIOS) {
+      status = response;
+    } else {
+      status = response.status;
+    }
 
-		if(status)
-			listener.apply(this, [status]);
-	}
+    if (status) listener.apply(this, [status]);
+  }
 });
 
-GPSState.addListener = (callback)=>{
-	if(typeof callback == 'function'){
-		listener = callback;
-		GPSState._startListen();
-	}
-}
+GPSState.addListener = (callback) => {
+  if (typeof callback === 'function') {
+    listener = callback;
+    GPSState._startListen();
+  }
+};
 
-GPSState.removeListener = (callback)=>{
-	if(subscription){
-		GPSState._stopListen();
-		subscription.remove();
-		subscription = null;
-	}
-}
+GPSState.removeListener = (callback) => {
+  if (subscription) {
+    GPSState._stopListen();
+    subscription.remove();
+    subscription = null;
+  }
+};
 
-GPSState.getStatus = ()=>{
-	return GPSState._getStatus();
-}
+GPSState.getStatus = () => GPSState._getStatus();
 
-GPSState.openSettings = ()=>{
-	GPSState._openSettings();
-}
+GPSState.openSettings = () => {
+  GPSState._openSettings();
+};
 
-GPSState.requestAuthorization = (authType)=>{
-	if(isIOS){
-		var type = parseInt(authType);
-		var min = GPSState.STATUS_NOT_DETERMINED;
-		var max = GPSState.STATUS_AUTHORIZED_WHENINUSE;
-		var inRange = (type>=min && type <= max);
+GPSState.requestAuthorization = (authType) => {
+  if (isIOS) {
+    let type = parseInt(authType);
+    const min = GPSState.STATUS_NOT_DETERMINED;
+    const max = GPSState.STATUS_AUTHORIZED_WHENINUSE;
+    const inRange = type >= min && type <= max;
 
-		if(isNaN(type) || !inRange){
-			type = GPSState.AUTHORIZED_WHENINUSE;
-		}
-		GPSState._requestAuthorization(type);
-	}else{
-		GPSState._requestAuthorization();
-	}
-}
+    if (isNaN(type) || !inRange) {
+      type = GPSState.AUTHORIZED_WHENINUSE;
+    }
+    GPSState._requestAuthorization(type);
+  } else {
+    GPSState._requestAuthorization();
+  }
+};
 
 module.exports = GPSState;
